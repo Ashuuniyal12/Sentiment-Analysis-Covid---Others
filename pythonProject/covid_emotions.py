2fb9276b950b5e5087aefa93139aefe50ad6256f
AdityaSahai123 <72221032+AdityaSahai123@users.noreply.github.com>
2022-02-09 16:35:05 +0530

    Initial commit

---
+++ pythonProject/covid_emotions.py
@@ -0,0 +1,13 @@
+﻿dic={
+    "0": "Optimistic",
+    "1": "Thankful",
+    "2": "Empathetic",
+    "3": "Pessimistic",
+    "4": "Anxious",
+    "5": "Sad",
+    "6": "Annoyed",
+    "7": "Denia",
+    "8": "Surprise",
+    "9": "Official-report",
+    "10": "Joking",
+}
