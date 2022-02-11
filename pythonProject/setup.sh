09e0d7132f7b85e95641a8b3202404414d4507c6
AdityaSahai123 <72221032+AdityaSahai123@users.noreply.github.com>
2022-02-11 20:12:32 +0530

    changes for web app

---
+++ pythonProject/setup.sh
@@ -0,0 +1,13 @@
+﻿mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"aniket.wattamwar17@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
